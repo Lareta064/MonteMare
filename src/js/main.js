d23c03b7362188accb09bfc42353e682de1bbca7
Lareta <lareta064@gmail.com>
2020-02-17 21:17:21 +0300

    слайдер

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    $('.section-slider').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
 
+        smartSpeed: 900,
+        dots: true,
+        nav: true,
+        navText: ["<i class='fas fa-arrow-left'/>", "<i class='fas fa-arrow-right'/>"],
+        URLhashListener: true,
+        autoplayHoverPause: true,
+        startPosition: 'URLHash'
+    });
 })
